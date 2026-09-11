de069c160ef58b406303d507b8e0c5020f418e31
RAJA6379 <rajavenkat2024@gmail.com>
2026-09-11 14:03:39 +0530

    faq page animation 0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,74 @@
+
 import {
   Route,
   Routes,
   useLocation,
-} from 'react-router-dom'
+} from "react-router-dom";
 
-import { Navbar } from './components/Navbar'
-import { Footer } from './components/Footer'
 
-import { Home } from './pages/Home'
-import { Platform } from './pages/Platform'
-import { Features } from './pages/Features'
-import { Build } from './pages/Build'
-import { Support } from './pages/Support'
-import { About } from './pages/About'
-import { Faq } from './pages/Faq'
+import { useEffect } from "react";
 
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+
+import { Home } from "./pages/Home";
+import { Platform } from "./pages/Platform";
+import { Features } from "./pages/Features";
+import { Build } from "./pages/Build";
+import { Support } from "./pages/Support";
+import { About } from "./pages/About";
+import { Faq } from "./pages/Faq";
 
 export default function App() {
-
-  const location = useLocation()
+  const location = useLocation();
 
   const isAboutPage =
-    location.pathname === '/about'
+    location.pathname === "/about";
 
+  // Scroll to top whenever page changes
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "auto",
+    });
+  }, [location.pathname]);
 
   return (
     <>
       <Navbar />
 
       <main>
-
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/platform" element={<Platform />} />
-          <Route path="/features" element={<Features />} />
-          <Route path="/build" element={<Build />} />
-          <Route path="/support" element={<Support />} />
-          <Route path="/faq" element={<Faq />} />
-          <Route path="/about" element={<About />} />
+          <Route
+            path="/platform"
+            element={<Platform />}
+          />
+          <Route
+            path="/features"
+            element={<Features />}
+          />
+          <Route
+            path="/build"
+            element={<Build />}
+          />
+          <Route
+            path="/support"
+            element={<Support />}
+          />
+          <Route
+            path="/faq"
+            element={<Faq />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
         </Routes>
-
       </main>
 
-
-      {/* Footer shows on other pages,
-          but NOT on About page */}
-
+      {/* Footer shows on all pages except About */}
       {!isAboutPage && <Footer />}
     </>
   );
